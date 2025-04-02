82840037a59bd3be4399fccfea1185b499bab031
Marcelo Rangel Barros <marcelorangelb2006@gmail.com>
2025-04-02 17:10:19 -0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Digite um número: "))
-
-if num < 0:
-    print("O número deve ser maior ou igual a 0: ")
-    num = int(input("Digite um número: "))
-for i in range (1, num + 1):
-    print(" ", i)   
